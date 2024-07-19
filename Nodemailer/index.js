4c53353e3e17c3ebbbf51c524af88d4215576f6e
bhowmik1234 <124621278+bhowmik1234@users.noreply.github.com>
2024-07-19 12:19:50 +0530

    completed nodemailer

---
+++ Nodemailer/index.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "",
+      pass: "",
+    },
+  });
+  
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '', // sender address
+      to: "user@gmail.com", // list of receivers
+      subject: "Registration", // Subject line
+      text: "Hello world?", // plain text body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  }
+  
+  main().catch(console.error);
